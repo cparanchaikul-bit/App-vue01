f8ad3c5ca6b93b896662d4c9ada05857eba0a7eb
Cparanchaikul-bit <cparanchaikul@gmail.com>
2026-01-11 18:33:35 +0700

    Initial commit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,43 @@
 <template lang="">
-    <div>
-<h1>contact to Chaipattirat paranchaikul </h1>
+    <div class="container"> 
+<h1 class="tect-primary">contact to Chaipattirat paranchaikul </h1>
 <h3>Tel.086-086-655</h3>
-<img alt="Vue logo" src="../assets/1.jpg">
+<img alt="Vue logo" src="../assets/2.jpg">
+
+<div class="container text-center " text="center">
+    
+  <div class="row ">
+    <div class="col-sm-6">
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+    <div class="col-sm-6">
+      <div class="container">
+<div class="mb-3 p-3 mb-2 bg-light text-dark ">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3 p-3 mb-2 bg-light text-dark ">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<div class="mb-3 text-center">
+<button type="button" class="btn btn-primary">ตกลง</button>
+<button type="button" class="btn btn-danger">ยกเลิก</button>
+</div>
+
+</div>
+
+    </div>
+   
+  </div>
+</div>
+
+
+
+
+
+
+
 
 
     </div>
